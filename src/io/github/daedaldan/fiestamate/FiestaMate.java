2659644f11742f82f18d54cd860cc9f8c580c277
Daniel Wang <daniel22373@gmail.com>
2023-05-30 12:11:27 -0400

    Test updated .gitignore

--- src/io/github/daedaldan/fiestamate/FiestaMate.java
+++ src/io/github/daedaldan/fiestamate/FiestaMate.java
@@ -2,6 +2,7 @@ package io.github.daedaldan.fiestamate;
 
 public class FiestaMate {
 	public static void main(String[] args) {
-		
+		// Test updated .gitignore
+		System.out.println("testing");
 	}
 }
