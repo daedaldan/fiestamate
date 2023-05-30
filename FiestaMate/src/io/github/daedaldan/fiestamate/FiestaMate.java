51d4045d56c5ea01995f6faf934f72752333fb3c
Daniel Wang <daniel22373@gmail.com>
2023-05-30 11:48:30 -0400

    Test moving .gitignore file

--- FiestaMate/src/io/github/daedaldan/fiestamate/FiestaMate.java
+++ FiestaMate/src/io/github/daedaldan/fiestamate/FiestaMate.java
@@ -2,7 +2,7 @@ package io.github.daedaldan.fiestamate;
 
 public class FiestaMate {
 	public static void main(String[] args) {
-		// testing
+		// testing, testing again
 		System.out.println("test");
 	}
 }
