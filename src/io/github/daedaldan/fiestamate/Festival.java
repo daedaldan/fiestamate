9363c18f1ef4eaa6d66a0455b33489a9dd67bfce
Daniel Wang <daniel22373@gmail.com>
2023-06-03 12:03:23 -0400

    Start code for reading CSV with event details

--- src/io/github/daedaldan/fiestamate/Festival.java
+++ src/io/github/daedaldan/fiestamate/Festival.java
@@ -1,11 +1,36 @@
 package io.github.daedaldan.fiestamate;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Festival {
 	private ArrayList<Event> festivalEvents = new ArrayList<Event>();
 	
-	public Festival() {
+	public Festival(String fileName) {
+		this.festivalEvents = processEventData(fileName);
+	}
+	
+	/*  
+	 * Uses Scanner to read event details stored in CSV file row-by-row.
+	 * Each row contains details for a single Event object.
+	 */
+	private ArrayList<Event> processEventData(String fileName) {
+		ArrayList<Event> events = new ArrayList<Event>();
 		
+		try (Scanner scan = new Scanner(fileName)) {
+			// skip header row
+			if (scan.hasNextLine()) {
+				scan.nextLine();
+			}
+			// process each row with event details
+			while (scan.hasNextLine()) {
+				System.out.println(scan.nextLine());
+			}
+		} catch (Exception e) {
+			System.out.println("The CSV file '" + fileName + "' is invalid.");
+		}
+		
+		
+		return events;
 	}
 }
