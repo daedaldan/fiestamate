6ec62d492232018273392bbef265954258300bb7
Daniel Wang <daniel22373@gmail.com>
2023-11-06 21:03:32 -0500

    Resize ListViews, allow user select multiple events

--- src/io/github/daedaldan/fiestamate/FiestaMate.java
+++ src/io/github/daedaldan/fiestamate/FiestaMate.java
@@ -12,10 +12,19 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+/**
+ * FiestaMate is a desktop GUI application that allows the user to browse festival event details loaded from a CSV file.
+ * 
+ * The user can add events from the festival to his/her itinerary and export his/her itinerary as a PDF.
+ * 
+ * @author danielwang
+ *
+ */
 public class FiestaMate extends Application {
 	// Create Festival object using CSV and TXT file paths.
 	private static Festival harvestDay = new Festival("/Users/danielwang/git/fiestamate/src/io/github/daedaldan/fiestamate/events.csv",
@@ -66,7 +75,9 @@ public class FiestaMate extends Application {
 				
 		// Initialize the ListView for the events list.
 		eventsListView = new ListView<>(eventsList);
-		eventsListView.setPrefHeight(200);
+		eventsListView.setPrefHeight(500);
+		// Allow the user to select multiple events from the list.
+		eventsListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 		
 		// Create heading text for the itinerary.
 		Text itineraryText = new Text("My Events");
@@ -74,7 +85,7 @@ public class FiestaMate extends Application {
 		
 		// Initialize the ListView for the itinerary list.
 		itineraryListView = new ListView<>(itineraryList);
-		itineraryListView.setPrefHeight(200);
+		itineraryListView.setPrefHeight(300);
 		
 		// Create a button to add events to the itinerary.
 		Button addButton = new Button("Add to My Events");
